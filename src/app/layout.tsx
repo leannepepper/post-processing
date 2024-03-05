0651dafee62ab6cc5dcf0c5cf6dfd33ec1f79ea6
Leanne Werner <leanne.werner.lw@gmail.com>
2024-03-04 23:52:34 -0800

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Post Processing",
+  viewport: { width: "device-width", initialScale: 1 },
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,48 @@ export default function Home() {
   useEffect(() => {
     size.width = window.innerWidth;
     size.height = window.innerHeight;
-    initScene();
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      size.width / size.height,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector("canvas")!,
+    });
+
+    renderer.setSize(size.width, size.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    camera.position.z = 5;
+    scene.add(camera);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+
+    scene.add(cube);
+    renderer.render(scene, camera);
+
+    function animate() {
+      renderer.render(scene, camera);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+    window.addEventListener("resize", () => onWindowResize(camera, renderer));
+
+    return () => {
+      window.removeEventListener("resize", () =>
+        onWindowResize(camera, renderer)
+      );
+    };
   }, []);
 
   return (
@@ -22,50 +63,51 @@ export default function Home() {
   );
 }
 
-function initScene() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("canvas")!,
-  });
-
+function initScene(
+  camera: THREE.PerspectiveCamera,
+  scene: THREE.Scene,
+  renderer: THREE.WebGLRenderer
+) {
   renderer.setSize(size.width, size.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
   camera.position.z = 5;
+  scene.add(camera);
 
   const geometry = new THREE.BoxGeometry();
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   const cube = new THREE.Mesh(geometry, material);
 
   scene.add(cube);
-  onWindowResize(camera, renderer);
   renderer.render(scene, camera);
 
-  // function animate() {
-  //   renderer.render(scene, camera);
+  function animate() {
+    renderer.render(scene, camera);
 
-  //   cube.rotation.x += 0.01;
-  //   cube.rotation.y += 0.01;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
-  //   requestAnimationFrame(animate);
-  // }
+    requestAnimationFrame(animate);
+  }
 
-  // animate();
-
-  window.addEventListener("resize", () => onWindowResize(camera, renderer));
+  animate();
 }
 
 function onWindowResize(
   camera: THREE.PerspectiveCamera,
   renderer: THREE.WebGLRenderer
 ) {
+  console.log("Resizing window");
+
   // Update sizes
   size.width = window.innerWidth;
   size.height = window.innerHeight;
 
+  console.log(`New size: ${size.width} x ${size.height}`);
+
   // Update camera
   camera.aspect = size.width / size.height;
+  console.log(`New camera aspect: ${camera.aspect}`);
   camera.updateProjectionMatrix();
 
   // Update renderer
